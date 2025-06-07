e19f08142e3dd0682e66cd9864a3cbf74495168c
dlhwang <kshw1357@gmail.com>
2025-06-07 20:56:18 +0900

    feat: 카달로그 엔티티 -> 도메인 변환메서드 작성 및 추가 값객체 생성

--- src/main/kotlin/com/dollee/market/dolleeMarket/catalog/infrastructure/Description.kt
+++ src/main/kotlin/com/dollee/market/dolleeMarket/catalog/infrastructure/Description.kt
@@ -1,5 +1,9 @@
 package com.dollee.market.dolleeMarket.catalog.infrastructure
 
+import com.dollee.market.dolleeMarket.catalog.domain.Description
+
 data class Description(
     val text: String
-)
+) {
+    fun toDomain(): Description = Description(text)
+}
