314addb01f30b78a936b5b171a6baf6b67300cfe
dlhwang <kshw1357@gmail.com>
2025-06-08 21:32:22 +0900

    feat: Product 생성 개발

--- src/main/kotlin/com/dollee/market/dolleeMarket/product/domain/Product.kt
+++ src/main/kotlin/com/dollee/market/dolleeMarket/product/domain/Product.kt
@@ -3,11 +3,11 @@ package com.dollee.market.dolleeMarket.product.domain
 import com.dollee.market.dolleeMarket.product.infrastructure.ProductEntity
 
 data class Product (
-    val id: String?,
+    val id: String?=null,
     val name: String,
     val price: Long
 ) {
     fun toEntity(): ProductEntity {
-        return ProductEntity(id, name, price)
+        return ProductEntity(name= name, price = price)
     }
 }
--- src/main/kotlin/com/dollee/market/dolleeMarket/product/service/ProductService.kt
+++ src/main/kotlin/com/dollee/market/dolleeMarket/product/service/ProductService.kt
@@ -1,7 +1,10 @@
 package com.dollee.market.dolleeMarket.product.service
 
 import com.dollee.market.dolleeMarket.application.dto.ProductListResponse
+import com.dollee.market.dolleeMarket.application.dto.toResponse
+import com.dollee.market.dolleeMarket.product.domain.Product
 import com.dollee.market.dolleeMarket.product.domain.ProductRepository
+import com.dollee.market.dolleeMarket.application.dto.CreateProductCommand
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,5 +14,9 @@ class ProductService(private val productRepository: ProductRepository) {
         return productRepository.findAll()
             .map { product -> ProductListResponse(product.id!!, product.name, product.price) }
             .toMutableList()
+    }
+
+    fun saveProduct(command : CreateProductCommand) : ProductListResponse{
+        return productRepository.save(Product(name =command.name,price = command.price)).toResponse()
     }
 }
